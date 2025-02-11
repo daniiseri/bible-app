53e6cbf8a3ef442b847e428ec732d09910576d27
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2025-02-11 15:18:24 -0300

    Feat - Add request bible api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
+'use client'
+
+import { api } from "@/lib/api";
+import { env } from "@/lib/env";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+
 export default function Home() {
+  const searchParams = useSearchParams()
+  const query = searchParams.get('query') || ''
+
+  useQuery({
+    queryKey: [query],
+    queryFn: async () => {
+      const response = await api(`${env.BIBLE_ID}/search?query=${query}`)
+
+      return response.json()
+    }
+  })
 
   return (
     <div>
